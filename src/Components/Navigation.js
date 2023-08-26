43681cea51ac6ac2bf4760f005db2318311bbdaa
Bezughly Michailo <bezughly.michailo@itechcraft.com>
2023-08-26 20:45:49 +0300

    +Add navigation

---
+++ src/Components/Navigation.js
@@ -0,0 +1,22 @@
+import './Header.css'
+
+const Navigation = () => {
+    return (<nav className="nav">
+        <ul>
+            <li>Home</li>
+            <li>About</li>
+            <li>Menu</li>
+            <li>Reservations</li>
+            <li>Order online</li>
+            <li>Login</li>
+        </ul>
+        {/*<a href="/">Home</a>*/}
+        {/*<a href="/about">About</a>*/}
+        {/*<a href="/Menu">Menu</a>*/}
+        {/*<a href="/Reservations">Reservations</a>*/}
+        {/*<a href="/OrderOnline">Order online</a>*/}
+        {/*<a href="/Login">Login</a>*/}
+    </nav>);
+}
+
+export default Navigation;
